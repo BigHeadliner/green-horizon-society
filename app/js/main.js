95486746285af2c2c9975675986c2afde3d1a25a
BigHeadliner <bigheadliner@gmail.com>
2023-11-20 22:27:41 +0200

    news section ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,16 @@
 $(function () {   
      
     
- 
+    document.querySelectorAll('.news__item').forEach(function(item) {
+        item.addEventListener('mouseover', function() {
+          this.classList.add('rotate');
+        });
+      
+        item.addEventListener('mouseout', function() {
+          this.classList.remove('rotate');
+        });
+      });
+      
 
   
  
