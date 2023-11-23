fc979fd398993ba7df6aa8fb813a6d8805fa910e
BigHeadliner <bigheadliner@gmail.com>
2023-11-23 21:54:40 +0200

    popup contact form ready

--- app/js/main.js
+++ app/js/main.js
@@ -8,28 +8,46 @@ $(function () {
       
         item.addEventListener('mouseout', function() {
           this.classList.remove('rotate');
-        });
-      });
+        }); 
+         
+      });   
+       
+
+      // Magnific Popup
+       
+      $('#openPopupBtn').magnificPopup({
+        items: {
+            src: '#myModal',
+            type: 'inline'
+        },
+        midClick: true,
+        closeOnBgClick: true // Дозволяє закривати попап при кліку на чорний фон
+    });
+
+    // Опціонально: закрийте модальне вікно після відправки форми
+    $('#myForm').submit(function(event) {
+        event.preventDefault();
+        $.magnificPopup.close();
+        // Додайте ваш код для обробки відправки форми
+    });
+
+    // Додайте обробник подій для кнопки закриття
+    $('.close-popup-btn, #myModal').on('click', function() {
+        $.magnificPopup.close();
+    });
+
+    // Запобігання закриванню попапу при кліках всередині форми
+    $('.mp-form-wrapper').on('click', function(event) {
+        event.stopPropagation();
+    });
+
+        
+});
       
 
   
- 
-  
- 
-  
-  
-  
-  
-     
-
- 
-
 
 
   
 
-
-  
-  
    
-});
